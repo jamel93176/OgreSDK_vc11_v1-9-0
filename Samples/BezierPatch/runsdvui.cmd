6877fb4efc991dd90428c9078922560bbd162d05
James Miller <jamel93176@gmail.com>
2016-11-24 08:15:28 -0600

    Add project files.

---
+++ Samples/BezierPatch/runsdvui.cmd
@@ -0,0 +1,2 @@
+cd /d "C:\Users\jamel\Documents\OgreSDK_vc11_v1-9-0\Samples\BezierPatch" &msbuild "Sample_BezierPatch.vcxproj" /t:sdvViewer /p:configuration="Release" /p:platform=Win32
+exit %errorlevel% 
